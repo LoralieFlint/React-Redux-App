762a4c09d9129e6d189c2155da0045ab92fcd339
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-10-24 18:17:08 -0600

    setup complete for index.js imports and setup

--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>,
+ document.getElementById('root'));
 
 
